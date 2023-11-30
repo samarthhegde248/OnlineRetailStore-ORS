0ead5e49582b1cc4963b32b756c54b942a80ef2c
samarthhegde248 <samarth.hegde248@gmail.com>
2023-11-30 17:17:52 +0530

    upgraded api gateway app

--- Edge-Eureka-Client/Edge-Eureka-Client/src/main/java/com/ORS/EdgeEurekaClient/EdgeEurekaClientApplication.java
+++ Edge-Eureka-Client/Edge-Eureka-Client/src/main/java/com/ORS/EdgeEurekaClient/EdgeEurekaClientApplication.java
@@ -2,11 +2,11 @@ package com.ORS.EdgeEurekaClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @RefreshScope
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EdgeEurekaClientApplication {
 
